6ebdb4508af6c9258924a48ce0e4e348479398a2
Malcolm <malcolm.newbury@guildfoss.com>
2025-01-23 22:40:49 +0000

    Z

--- Electtrek.py
+++ Electtrek.py
@@ -138,8 +138,9 @@ class TreeNode:
 #      self.map.fit_bounds(self.get_bounding_box(block), padding = (20, 20))
 # the folium save file reference must be a url
       nodemapfile = self.dir+"/"+self.file
-      self.map.add_css_link("electtrekcss","https://newbrie.github.io/Electtrek/static/print.css")
-      self.map.add_css_link("electtrekcss","https://newbrie.github.io/Electtrek/static/style.css")
+      self.map.add_css_link("electtrekprint","https://newbrie.github.io/Electtrek/static/print.css")
+      self.map.add_css_link("electtrekstyle","https://newbrie.github.io/Electtrek/static/style.css")
+      self.map.add_css_link("electtrekelectorwalks","https://newbrie.github.io/Electtrek/static/electorwalks.js")
 
       target = self.locmappath("")
       self.map.save(target)
