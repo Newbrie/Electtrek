5b0e008616a93d3650df1375e15416e297d628b2
Malcolm <malcolm.newbury@guildfoss.com>
2025-01-23 23:26:47 +0000

    Z

--- static/map.js
+++ static/map.js
@@ -1,8 +1,7 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 
-document.getElementById(
-    'down-button').addEventListener('click', (e) => {
+function reftag() {
         window.parent.location.reload();
         console.log("down-button has been clicked");
     });
