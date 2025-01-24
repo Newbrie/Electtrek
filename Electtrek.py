616a601512d1879e689db6a3bde96631084905b4
Malcolm <malcolm.newbury@guildfoss.com>
2025-01-24 01:08:47 +0000

    Z

--- Electtrek.py
+++ Electtrek.py
@@ -343,8 +343,9 @@ class FGlayer:
                         limb = gpd.GeoDataFrame(df, geometry= [convex], crs="EPSG:4326")
 
                     if herenode.level == 0:
+                        url = url_for('map',c.dir+"/"+c.file)
                         downtag = "<form action= '/downcountbut/{0}' ><button type='submit' id='down-button' style='font-size: {2}pt;color: gray'>{1}</button></form>".format(c.dir+"/"+c.file,"COUNTIES",12)
-                        reftag = "<button onclick='reftag()' style='font-size: {2}pt;color: gray'>{1}</button>".format(c.dir+"/"+c.file,"REFRESH",12)
+                        reftag = "<button onclick='reftag({0})' style='font-size: {2}pt;color: gray'>{1}</button>".format(url,"REFRESH",12)
                         limb['UPDOWN'] = "<br>"+c.value+"<br>"+ reftag + downtag
                         c.tagno = len(self.children)+1
                         print("_________new child boundary value and tagno:  ",c.value, c.tagno)
@@ -1202,6 +1203,7 @@ def map(path):
     flash ("_________Nextmap"+path)
     print ("_________Nextmap",path)
     return send_from_directory(app.config['UPLOAD_FOLDER'],path, as_attachment=False)
+
 
 @app.route('/upload', methods=['POST','GET'])
 def upload():
--- static/map.js
+++ static/map.js
@@ -1,7 +1,7 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 
-function reftag() {
-  window.parent.location.href = "http://127.0.0.1:5000/index";
+function reftag(url) {
+  window.parent.location.href = url;
   console.log("down-button has been clicked");
     };
