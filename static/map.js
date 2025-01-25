c185339d48fa3372b9947d06763a1618f0102305
Malcolm <malcolm.newbury@guildfoss.com>
2025-01-25 15:13:44 +0000

    Z

--- static/map.js
+++ static/map.js
@@ -2,6 +2,7 @@
 toggle between hiding and showing the dropdown content */
 
 function reftag(url) {
-  window.parent.location.href = "http://127.0.0.1:5000/map/UNITED_KINGDOM/ENGLAND/ENGLAND-MAP.html";
   console.log("down-button has been clicked");
+  window.parent.location.href = "http://127.0.0.1:5000/map/UNITED_KINGDOM/ENGLAND/ENGLAND-MAP.html";
+
     };
