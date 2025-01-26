b97dfb55a80be4ab64aa2563b077111209a235d6
Malcolm <malcolm.newbury@guildfoss.com>
2025-01-26 10:00:42 +0000

    Update map.js

--- static/map.js
+++ static/map.js
@@ -3,7 +3,7 @@ toggle between hiding and showing the dropdown content */
 
 var moveDown = function (msg) {
   // Send a message to the parent
-      window.parent.postMessage("fetching from "+ msg, '*');
+      window.parent.postMessage("Fetching data about "+ msg, '*');
       alert("sent:"+msg);
-      window.location.href = msg;
+      window.location.assign = msg;
       };
