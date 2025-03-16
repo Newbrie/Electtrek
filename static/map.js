85df35a2c3c90ae0ec1a342311025e55f04b7cfb
Malcolm <malcolm.newbury@guildfoss.com>
2025-03-16 14:00:58 +0000

    Z

--- static/map.js
+++ static/map.js
@@ -6,21 +6,23 @@ var moveDown = function (msg,area, type) {
       window.parent.postMessage("Drilling down to "+type+ " set within "+ area, '*');
       window.location.assign(msg);
       var ul = parent.document.getElementById("logwin");
-      ul.scrollTop = 0;
+      ul.scrollTop = ul.scrollHeight;
       };
 var moveUp = function (msg,area, type) {
   // Send a message to the parent
       window.parent.postMessage("Moving up to "+ area + " "+ type+ " level ", '*');
       window.location.assign(msg);
       var ul = parent.document.getElementById("logwin");
-      ul.scrollTop = 0;
+      ul.scrollTop = ul.scrollHeight;
+
       };
 var showMore = function (msg,area, type) {
   // Send a message to the parent
       window.parent.postMessage("Showing the "+type+ " set within "+ area, '*');
       window.location.assign(msg);
       var ul = parent.document.getElementById("logwin");
-      ul.scrollTop = 0;
+      ul.scrollTop = ul.scrollHeight;
+
       };
 
 async function displayXURL(url) {
