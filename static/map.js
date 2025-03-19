9a66032a8106c6172305a0b6d5e2ae617b002a04
Malcolm <malcolm.newbury@guildfoss.com>
2025-03-19 11:38:23 +0000

    Z

--- static/map.js
+++ static/map.js
@@ -30,3 +30,17 @@ var layerUpdate = function () {
       var ul = parent.document.getElementById("logwin");
       ul.scrollTop = ul.scrollHeight;
       };
+
+function highlightNumber(selector) {
+    document.querySelectorAll(selector).forEach(element => {
+        let text = element.innerHTML;
+        let match = text.match(/\d+/); // Find the first number in the text
+
+        if (match) {
+            let highlighted = `<span class="highlight">${match[0]}</span>`;
+            element.innerHTML = text.replace(match[0], highlighted); // Replace number with highlighted version
+        }
+    });
+}
+
+highlightNumber("leaflet-marker-icon leaflet-div-icon leaflet-zoom-animated leaflet-interactive");
