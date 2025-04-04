05276f3b9331e65287fdd570fe4030429c77a6d7
Malcolm <malcolm.newbury@guildfoss.com>
2025-04-04 08:42:13 +0100

    Update map.js

--- static/map.js
+++ static/map.js
@@ -36,7 +36,6 @@ var showMore = function (msg,area, type) {
         inputs.forEach(input => {
 
             let row = input.closest("tr"); // Get the closest row
-            alert(input+"|"+row );
             let electorID = row.cells[1].innerText.trim(); // Assuming 'ENOP' is in the second column
             let ElectorName = row.cells[2].innerText.trim(); // Get input value
             let viValue = row.cells[7].innerText.trim(); // Get input value
