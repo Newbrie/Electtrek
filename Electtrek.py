623d90fb28faf4a3b6c79f2192d3b1d94fcde3cb
Malcolm <malcolm.newbury@guildfoss.com>
2025-12-12 00:09:09 +0000

    Update Electtrek.py

--- Electtrek.py
+++ Electtrek.py
@@ -7402,7 +7402,6 @@ def get_table(table_name):
             print(f"____NODE: {current_node.findnodeat_Level(lev)} tabtype {tabtype} listNUM {len(current_node.findnodeat_Level(lev).childrenoftype(tabtype))}  ")
             [column_headers,rows, title] = get_layer_table(current_node.findnodeat_Level(lev).childrenoftype(tabtype), str(tabtype)+"s")
             print(f"____NODELOOKUP {table_name} -COLS {column_headers} ROWS {rows} TITLE {title}")
-            raise Exception("WINDSOR")
             return jsonify([column_headers, rows.to_dict(orient="records"), title])
         elif table_name.endswith("_xref"):
             lev = current_node.level+1
@@ -7422,7 +7421,6 @@ def get_table(table_name):
         print(f"____TABLENAME {table_name} -COLS {column_headers} ROWS {rows} TITLE {title}")
         return jsonify([column_headers, rows, title])
     except Exception as e:
-        raise Exception("WINDSOR")
         print(f"[ERROR] Failed to generate table '{table_name}': {e}")
         return jsonify({"error": str(e)}), 500
 
